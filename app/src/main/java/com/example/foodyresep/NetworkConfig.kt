7a978dad908357404a79c2edd250a0dfb7b29b69
riantwahydi <20523100@students.uii.ac.id>
2022-12-12 15:42:48 +0700

    Favorite

--- app/src/main/java/com/example/foodyresep/NetworkConfig.kt
+++ app/src/main/java/com/example/foodyresep/NetworkConfig.kt
@@ -1,6 +1,8 @@
 package com.example.foodyresep
 
 import com.example.foodyresep.data.UserResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -10,22 +12,25 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 
 class NetworkConfig {
-//    fun getInterceptor() : OkHttpClient {
-//        val logging = HttpLoggingInterceptor()
-//        logging.level = HttpLoggingInterceptor.Level.BODY
-//        val okHttpClient = OkHttpClient.Builder()
-//            .addInterceptor(logging)
-//            .build()
-//        return  okHttpClient
-//    }
-//    fun getRetrofit() : Retrofit {
-//        return Retrofit.Builder()
-//            .baseUrl("https://foodyresep.000webhostapp.com/")
-//            .client(getInterceptor())
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//    }
-//    fun getService() = getRetrofit().create(Users::class.java)
+    fun getService(): Any {
+
+    }
+    fun getInterceptor() : OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+        return  okHttpClient
+    }
+    fun getRetrofit() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://foodyresep.000webhostapp.com/")
+            .client(getInterceptor())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    fun getService(): Users = getRetrofit().create(Users::class.java)
 }
 interface Users {
     @FormUrlEncoded
