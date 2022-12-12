19c2c551ffdb88f0f29b51a090bc3e719b81ef6a
jokosusiloo <susilo30joko@gmail.com>
2022-12-12 14:33:51 +0700

    add

--- app/src/main/java/com/example/foodyresep/LoginActivity.kt
+++ app/src/main/java/com/example/foodyresep/LoginActivity.kt
@@ -1,11 +1,54 @@
 package com.example.foodyresep
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.foodyresep.data.UserResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var editTextTextEmailAddress: EditText
+    private  lateinit var editTextTextPassword: EditText
+    private lateinit var bLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress) as EditText
+        editTextTextPassword = findViewById(R.id.editTextTextPassword) as EditText
+        bLogin = findViewById(R.id.bLogin) as Button
+        bLogin.setOnClickListener{
+            if (editTextTextEmailAddress.text.equals("") || editTextTextPassword.text.equals("")){
+                Toast.makeText(this, "Data Masih Ada Yang Kosong", Toast.LENGTH_SHORT).show()
+
+            }else{
+                login(editTextTextEmailAddress.text.toString(), editTextTextPassword.text.toString())
+            }
+        }
+
+
+    }
+
+    private fun login(email: String, pass:String) {
+        NetworkConfig().getService()
+            .login(email, pass)
+            .enqueue(object : Callback<UserResponse> {
+                override fun onFailure(call: Call<UserResponse>, t: Throwable) {
+                    Toast.makeText(this@LoginActivity, t.localizedMessage, Toast.LENGTH_SHORT).show()
+                }
+                override fun onResponse(
+                    call: Call<UserResponse>,
+                    response: Response<UserResponse>
+                ) {
+                    startActivity(Intent(applicationContext, MainActivity::class.java))
+                }
+            })
     }
 }
---
+++ app/src/main/java/com/example/foodyresep/NetworkConfig.kt
@@ -0,0 +1,45 @@
+package com.example.foodyresep
+
+import com.example.foodyresep.data.UserResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Field
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+class NetworkConfig {
+    fun getInterceptor() : OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+        return  okHttpClient
+    }
+    fun getRetrofit() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://foodyresep.000webhostapp.com/")
+            .client(getInterceptor())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    fun getService() = getRetrofit().create(Users::class.java)
+}
+interface Users {
+    @POST("login.php")
+    fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<UserResponse>
+
+    @POST("register.php")
+    fun register(
+        @Field("email") email: String,
+        @Field("password") password: String,
+        @Field("usernmae") username: String
+    ): Call<UserResponse>
+
+}
